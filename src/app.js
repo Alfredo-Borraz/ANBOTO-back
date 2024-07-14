3e8664fa5309dc0fbbe25d4359126ac9a8cf7ecc
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-07-14 13:33:43 -0600

    Creacion de CRUD para la tabla producto, de la base de datos anboto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import productoRoutes from './routes/producto.routes.js';
 import usuarioAdminRoutes from './routes/usuarioAdmin.routes.js';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api/usuario_admin', usuarioAdminRoutes);
+app.use('/api/producto', productoRoutes);
 
 export default app;
 
