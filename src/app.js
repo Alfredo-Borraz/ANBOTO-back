d32198faac4b1e10a72ac130b2e831f393f22ca7
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-07-14 13:56:24 -0600

    Creacion de CRUD para la tabla plan_suscripcion, de la base de datos anboto

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import planSuscripcionRoutes from './routes/planSuscripcion.routes.js';
 import productoRoutes from './routes/producto.routes.js';
 import usuarioAdminRoutes from './routes/usuarioAdmin.routes.js';
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api/usuario_admin', usuarioAdminRoutes);
 app.use('/api/producto', productoRoutes);
+app.use('/api/plan_suscripcion', planSuscripcionRoutes);
+
 
 export default app;
 
