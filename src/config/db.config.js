016a03ea28f123f97a7c4074cd0ae79afd589fb1
Alfredo-Borraz <69995433+Alfredo-Borraz@users.noreply.github.com>
2024-07-16 23:05:35 -0600

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,6 @@
 const dbConfig = {
   HOST: process.env.DB_HOST,
-  PORT: process.env.PORT,
+  PORT: process.env.DB_PORT,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
