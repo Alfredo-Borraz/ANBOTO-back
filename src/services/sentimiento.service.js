100644bdc2412d87023e0e88bfa55d31b696b0c0
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-07-29 20:18:18 -0600

    Añadiendo Api URL de análisis de sentimiento

--- src/services/sentimiento.service.js
+++ src/services/sentimiento.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SENTIMENT_API_URL = 'http://127.0.0.1:5000/sentiment';
+const SENTIMENT_API_URL = 'https://analisis-sentimiento.onrender.com/sentiment';
 
 export const analyzeSentiment = async (text) => {
   try {
